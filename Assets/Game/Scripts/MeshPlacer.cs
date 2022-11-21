c08053403f822ee5207d1c8a76feb906948e076a
Ilya Korsak <sn026803310@gmail.com>
2022-11-21 07:07:03 +0200

    Moving and folowing
    Moving, folowing WIP

--- Assets/Game/Scripts/MeshPlacer.cs
+++ Assets/Game/Scripts/MeshPlacer.cs
@@ -5,10 +5,25 @@ using UnityEngine.InputSystem;
 
 public class MeshPlacer : MonoBehaviour
 {
+	private Coroutine movingCoroutine;
+	private IEnumerator SmoothLerp(Vector2 direction)
+	{
+		Vector3 startingPos = transform.position;
+		Vector3 finalPos = transform.position + (transform.forward * 5);
+		float elapsedTime = 0;
+
+		while (elapsedTime < time)
+		{
+			transform.position = Vector3.Slerp(startingPos, finalPos, (elapsedTime / time));
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+	}
 	public void OnMove(InputAction.CallbackContext context){
 		Vector2 inputVector = context.ReadValue<Vector2>();
 		Vector3 movementVectr = new Vector3(inputVector.x, 0, inputVector.y);
-		Ray ray = new Ray(transform.TransformPoint(movementVectr), -transform.up);
+		Ray ray = new Ray(transform.TransformPoint
+			(movementVectr), -transform.up);
 		RaycastHit raycastHit;
 		if (Physics.Raycast(ray, out raycastHit))
 		{
