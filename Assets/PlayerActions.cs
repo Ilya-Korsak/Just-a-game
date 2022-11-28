f2a614d512124a4896e76696c7a6643f37d1d7b4
Ilya Korsak <sn026803310@gmail.com>
2022-11-28 23:07:30 +0200

    Finished task 2 mechanics
    WIP: add more objects

---
+++ Assets/PlayerActions.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    [SerializeField] ParticleSystem actionParticleSystem;
+    [SerializeField] float actionDuration = 1.0f;
+    private Coroutine delayCoroutine = null;
+
+    IEnumerator ActionDelayCoroutine(Transform lookTo)
+    {
+        actionParticleSystem.Play();
+        float delayTeime = 0;
+        while (delayTeime < actionDuration)
+        {
+            delayTeime += Time.deltaTime;
+            actionParticleSystem.transform.LookAt(lookTo);
+            yield return new WaitForEndOfFrame();
+        }
+        actionParticleSystem.Stop();
+        delayCoroutine = null;
+    }
+
+    public bool PerformAction(Transform lookTo)
+    {
+        if (delayCoroutine == null)
+        {
+            delayCoroutine = StartCoroutine(ActionDelayCoroutine(lookTo));
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
---
+++ Assets/TorchTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform objectToLookAt;
+    [SerializeField] private Animator animator;
+    private bool isOnFire = false;
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!isOnFire && collider.gameObject.tag == "Player")
+        {
+            PlayerActions playerActions = collider.gameObject.GetComponent<PlayerActions>();
+            if (playerActions != null && playerActions.PerformAction(objectToLookAt))
+            {
+                animator.SetTrigger("isActivating");
+                isOnFire = true;
+            }
+        }
+    }
+}
